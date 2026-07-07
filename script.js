a1185c8e05fedd58aeaebdf54a1b1275fdecfde6
pinkLemon3 <meriblu1211@gmail.com>
2026-07-07 19:25:15 +0100

    Add an event listener that changes the colour of the squares when hovering over them.

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 let container = document.querySelector(".container");
+// Create the divs dynamically
 for (let i = 0; i < 256; i++) {
     let div = document.createElement("div");
     div.classList.add("square");
+    div.addEventListener("mouseover", () => {
+        div.classList.add("active");
+    })
     container.appendChild(div);
     console.log(div)
 }
