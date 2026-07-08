f33daffa0f0279bfa978445479a8967adfb20fd3
pinkLemon3 <meriblu1211@gmail.com>
2026-07-08 19:47:32 +0100

    Add dynamic grid generation and resizing

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 let container = document.querySelector(".container");
+let button = document.querySelector("button");
+createGrid(16);
 // Create the divs dynamically
-for (let i = 0; i < 256; i++) {
-    let div = document.createElement("div");
-    div.classList.add("square");
-    div.addEventListener("mouseover", () => {
-        div.classList.add("active");
-    })
-    container.appendChild(div);
-    console.log(div)
+function createGrid(sides) {
+    for (let i = 0; i < sides ** 2; i++) {
+        let div = document.createElement("div");
+        let squareSize = 960 / sides;
+        div.classList.add("square");
+        div.style.width = `${squareSize}px`;
+        div.style.height = `${squareSize}px`;
+        div.addEventListener("mouseover", () => {
+            div.classList.add("active");
+        })
+        container.appendChild(div);
+    }
 }
+
+// Add event listener to the button element
+button.addEventListener("click", () => {
+    let sides =
+        Number(prompt("How many squares per/side should the new grid have?"));
+    if (sides < 1 || sides > 100) {
+        return alert(`Please select a number inbetween 1-100! `);
+    }
+    container.innerHTML = "";
+    createGrid(sides);
+
+})
